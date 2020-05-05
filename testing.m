ebe908d830f2936f7793c4e9c7e029b611e9c1d9
srdja <srdja@DESKTOP-I57FJ7B.mshome.net>
2020-05-05 14:31:03 +1000

    Improved Precision

--- testing.m
+++ testing.m
@@ -8,7 +8,7 @@ window_size = 6e-3*Fs;
 threshold = 4*median(abs(X))/0.6745;
 cut = 0;
 isolated = 1;
-to_plot = 1;
+to_plot = 0;
 
 %% DETECT SPIKES
 [spikes, index, window_size, isolated_logical] = GetSpikes(X,window_size,threshold,cut,isolated);
@@ -18,6 +18,9 @@ isolated_index = index(isolated_logical);
 
 overlapped_spikes = spikes(not(isolated_logical),:);
 overlapped_index = index(not(isolated_logical));
+
+
+
 
 %% ISOLATED CLUSTERING
 [coeff,score,latent] = pca(isolated_spikes);
@@ -32,9 +35,9 @@ isolated_label = kmeans(features,3);
 
 %% TEMPLATE MATCHING
 
-error = 10;
+template_combined = [overlapped_template; template];
 
-[overlapped_label,PsC_score] = CorrelationMatching(overlapped_spikes,overlapped_template);
+[overlapped_label,PsC_score] = CorrelationMatching(overlapped_spikes,template_combined);
 
 P = perms(1:max(isolated_label));
 P = [P(:,end-1:end); [1:max(isolated_label); 1:max(isolated_label)]'];
@@ -47,16 +50,32 @@ for d=0:(max(isolated_label))^2 - 1
             label_shifted(e) = P(d+1,2)';
             label_detected(e) = P(d+1,1)';
         end
+        
+        if overlapped_label(e) == (d+1)*window_size + 1;
+            label_detected(e) = 1;
+        end
+        
+        if overlapped_label(e) == (d+1)*window_size + 2;
+            label_detected(e) = 2;
+        end
+        
+        if overlapped_label(e) == (d+1)*window_size + 3;
+            label_detected(e) = 3;
+        end
+        
     end
 end
 
 for j = 1:length(overlapped_index)
     
-    if overlapped_locations(overlapped_label(j)) > window_size/2
+    if overlapped_label(j) <= length(overlapped_locations)
         
-        index_shifted(j) = overlapped_index(j) + overlapped_locations(overlapped_label(j)) - window_size/2;
-    else
-        index_shifted(j) = overlapped_index(j) + overlapped_locations(overlapped_label(j));
+        if overlapped_locations(overlapped_label(j)) > window_size/2
+            
+            index_shifted(j) = overlapped_index(j) + overlapped_locations(overlapped_label(j)) - window_size/2;
+        else
+            index_shifted(j) = overlapped_index(j) + overlapped_locations(overlapped_label(j));
+        end
     end
 end
 
@@ -78,7 +97,7 @@ for i = 1:50
     subplot(2,1,1)
     plot(overlapped_spikes(i,:))
     subplot(2,1,2)
-    plot(overlapped_template(overlapped_label(i),:))
+    plot(template_combined(overlapped_label(i),:))
 end
 
 
